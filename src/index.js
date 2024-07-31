fc6bfa5514cefe652c86e8f3d64fa4010ce63521
EEJefferson <161738186+EEJefferson@users.noreply.github.com>
2024-07-31 15:58:56 +0300

    feat: add new code

--- src/index.js
+++ src/index.js
@@ -38,9 +38,31 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    let edge = '';
+    for (let i = 0; i < expr.length; i += 10) {
+        const part = expr.substring(i, i + 10);
+        let semi = '';
+        if (part === "**********") {
+            edge += ' ';
+        } else {
+            for (let j = 0; j < part.length; j += 2) {
+                if (part.substring(j, j + 2) !== "00" && part.substring(j, j + 2) === "10") {
+                    semi += ".";
+                } else if (part.substring(j, j + 2) !== "00" && part.substring(j, j + 2) === "11") {
+                    semi += "-";
+                }
+            }
+            result += semi;
+            if (MORSE_TABLE[result]) {
+                edge += MORSE_TABLE[result];
+                result = '';
+            }
+        }
+    }
+    return edge;
 }
 
 module.exports = {
     decode
-}
+};
